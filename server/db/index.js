6fa33f93d2414caf6b9b232ebc34d8988abf006f
Patrick Kenny <patrickmkenny91@gmail.com>
2023-01-11 11:07:37 -0600

    added associations

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,46 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Orders = require('./models/Products')
+const Products = require('./models/Products')
+const CreditCard = require('./models/CreditCard')
+const Cart = require('./models/Cart')
+
 
 //associations could go here!
+
+// User Associations 
+User.hasMany.Orders
+User.hasOne.Cart
+User.hasOne.CreditCard
+
+// Cart Associations 
+Cart.hasMany.Products
+Cart.BelongsTo.User
+
+// Orders Associations 
+Orders.belongsTo.User
+Orders.hasMany.Products
+
+// CreditCard Assocation
+CreditCard.belongsTo.User
+
+// Products Association
+Products.belongsToMany.Cart
+Products.belongsToMany.Orders
+
+
+
+
+
 
 module.exports = {
   db,
   models: {
     User,
+    Orders,
+    Products,
+    CreditCard,
+    Cart,
   },
 }
