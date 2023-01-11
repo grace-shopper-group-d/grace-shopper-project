6e3cd675bd3c2fd58fe2662b802bf06078ade07e
Patrick Kenny <patrickmkenny91@gmail.com>
2023-01-11 11:38:26 -0600

    added associations

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,6 @@
 
 const db = require("./db");
 const User = require("./models/User");
-
-const User = require('./models/User')
 const Orders = require('./models/Products')
 const Products = require('./models/Products')
 const CreditCard = require('./models/CreditCard')
