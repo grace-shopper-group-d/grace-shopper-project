267f5a3d176d81f18d382fee8d9dcfabc64d1d5f
Kevin Taylor <kevintaylor&84@gmail.com>
2023-01-11 12:43:21 -0500

    cart and User models

--- server/db/index.js
+++ server/db/index.js
@@ -3,9 +3,6 @@
 const db = require('./db')
 
 const User = require('./models/User')
-const Cart = require('./models/Cart')
-const Products = require('./models/Products')
-const Orders = require('./models/Orders')
 
 //associations could go here!
 
@@ -13,9 +10,6 @@ module.exports = {
   db,
   models: {
     User,
-    Cart,
-    Products,
-    Orders
 
   },
 }
