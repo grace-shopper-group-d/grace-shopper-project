9dbab6dbd72e5f52652b025ba65e1cd1f4595c90
Kevin Taylor <kevintaylor&84@gmail.com>
2023-01-11 12:36:20 -0500

    updated cart and user models

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,9 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Cart = require('./models/Cart')
+const Products = require('./models/Products')
+const Orders = require('./models/Orders')
 
 //associations could go here!
 
@@ -10,6 +13,9 @@ module.exports = {
   db,
   models: {
     User,
-    
+    Cart,
+    Products,
+    Orders
+
   },
 }
