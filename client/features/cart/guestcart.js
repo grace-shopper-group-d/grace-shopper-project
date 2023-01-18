b3d1efc865ba18e6631437305549168f7da46b36
Patrick Kenny <patrickmkenny91@gmail.com>
2023-01-17 18:34:00 -0600

    added guest cart and capabilities to add/edit/delete

--- client/features/cart/guestcart.js
+++ client/features/cart/guestcart.js
@@ -8,68 +8,76 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const GuestCart = () => {
 
-
-
+  const [render, setRender] = useState(false)
 
   let cart = window.localStorage.getItem('cart');
-  console.log(cart)
+  cart = JSON.parse(cart)
 
-  const dispatch = useDispatch()
+
 
   // increases cart item quantity
-  const handleQuantityIncrease = ({ cartId, userId, productId, cartQuantity }) => {
-   console.log('guest cart increase')
+  const handleQuantityIncrease = (product) => {
+    let idx = cart.indexOf(product);
+    cart[idx].cartQuantity++;
+    window.localStorage.setItem('cart', JSON.stringify(cart));
+    setRender(!render)
   }
 
   // decreases cart item quantity
-  const handleQuantityDecrease = ({ cartId, userId, productId, cartQuantity }) => {
-    console.log('guest cart decrease')
+  const handleQuantityDecrease = (product) => {
+    let idx = cart.indexOf(product);
+    if (cart[idx].cartQuantity > 1)
+    cart[idx].cartQuantity--;
+    window.localStorage.setItem('cart', JSON.stringify(cart));
+    setRender(!render)
   }
 
   // handles item delete
-  const handleItemDelete = (cartId) => {
-    console.log('guest cart delete')
+  const handleItemDelete = (product) => {
+    let idx = cart.indexOf(product);
+    let editedCart = cart.filter(currentProduct => currentProduct !== product);
+    console.log(editedCart)
+    window.localStorage.setItem('cart', JSON.stringify(editedCart));
+    setRender(!render)
   }
 
 
   // useEffect
   useEffect(() => {
 
-  }, [dispatch])
-
+  }, [render])
 
 
   return (
     <>
-    <div>Test</div>
-      {/* <button className='checkout-button'>Proceed To Checkout</button>
+      <button className='checkout-button'>Proceed To Checkout</button>
       <h3 id='cart-header' >Guest Cart</h3>
       <div id='cart-columns'>
-        <div  className='cart-column-photo'>Photo</div>
-        <div  className='cart-column-name'>Name</div>
+        <div className='cart-column-photo'>Photo</div>
+        <div className='cart-column-name'>Name</div>
         <div className='cart-column-price'>Price</div>
         <div className='cart-column-quantity'>Quantity</div>
         <div className='cart-column-total'>Total</div>
         <div className='cart-column-delete'>Delete</div>
       </div>
       <div id='cart-items-container'>
-        {cartProducts && cartProducts.length ?
-          cartProducts.map((product) => (
+        {cart && cart.length ?
+          cart.map((product) => (
             <div key={`cart-item-${product.id}`} className='cart-item-container'>
               <img key={`cart-item-img-${product.id}`} className="cart-item-img" src={product.imageUrl} />
               <div key={`cart-item-name-${product.id}`} className='cart-item-name'>{`${product.name}`}</div>
               <div key={`cart-item-price-${product.id}`} className='cart-item-price'>{`$${product.price}`}</div>
               <div key={`cart-item-quantity-${product.id}`} className='cart-quantity-container'>
-                <img onClick={(e) => handleQuantityIncrease(product.cart)} src='blackplus.png' height='16px' />
-                <div className='cart-item-total'>{product.cart.cartQuantity}</div>
-                <img onClick={(e) => handleQuantityDecrease(product.cart)} src='blackminus.png' height='16px' />
+                <img onClick={(e) => handleQuantityIncrease(product)} src='blackplus.png' height='16px' />
+                <div className='cart-item-total'>{product.cartQuantity}</div>
+                <img onClick={(e) => handleQuantityDecrease(product)} src='blackminus.png' height='16px' />
               </div>
 
-              <div id={`cart-item-total-${product.id}`} className='cart-item-price' >{`$${(product.cart.cartQuantity * product.price).toFixed(2)}`}</div>
-              <img id={`cart-item-delete-${product.id}`} onClick={(e) => handleItemDelete(product.cart.cartId)} src='reddelete.png' />
+              <div id={`cart-item-total-${product.id}`} className='cart-item-price' >{`$${(product.cartQuantity * product.price).toFixed(2)}`}</div>
+              <img onClick={(e) => handleItemDelete(product)} id={`cart-item-delete-${product}`} src='reddelete.png' />
             </div>)) : <h2 className='empty-cart'>Your Cart is Empty!</h2>}
         <div></div>
-      </div> */}
+      </div>
     </>
 
   )
--- client/features/singleproduct/singleProduct.js
+++ client/features/singleproduct/singleProduct.js
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { addCartAsync } from '../cart/cartSlice';
 import { fetchProduct, singleProduct } from './singleProductSlice';
-import {auth } from '../auth/authSlice'
+import { auth } from '../auth/authSlice'
 
 
 
 const SingleProduct = () => {
     const { productId } = useParams();
+    const navigate = useNavigate()
     const product = useSelector(singleProduct);
     const userId = useSelector((state) => state.auth.me.id);
+    const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+
 
 
 
     const dispatch = useDispatch();
 
+
+    // adds product to user through cart model 
     const handleAddToCart = (userId, productId) => {
         let newCart = {
             userId: userId,
@@ -23,30 +28,32 @@ const SingleProduct = () => {
             cartQuantity: 1,
         }
         dispatch(addCartAsync(newCart))
+        navigate('/cart')
     }
 
-   const handleAddtoCartGuest = () => {
-    let currentCart = window.localStorage.getItem('cart');
-    currentCart = JSON.parse(currentCart)
-    if (currentCart !== []) {
-        let newEmptyCart = [];
-        window.localStorage.setItem('cart', JSON.stringify(newEmptyCart))
-    }
-    let cartItem = {
-        description: product.description,
-        id: product.id,
-        imageUrl: product.imageUrl,
-        name: product.name,
-        price: product.price,
-        quantity: product.quantity,
-        cartQuantity: 1,
-    }
-       let cart = window.localStorage.getItem('cart');
-       cart = JSON.parse(cart)
-       console.log("current Cart", cart)
-       cart.push(cartItem);
-       console.log('updated Cart', cart)
-       window.localStorage.setItem(JSON.stringify(cart))
+    // Adds product to guest cart which is located in local storage
+    const handleAddtoCartGuest = () => {
+        let currentCart = window.localStorage.getItem('cart');
+        currentCart = JSON.parse(currentCart)
+        if (!currentCart) {
+            let newEmptyCart = [];
+            window.localStorage.setItem('cart', JSON.stringify(newEmptyCart))
+        }
+        let cartItem = {
+            description: product.description,
+            id: product.id,
+            imageUrl: product.imageUrl,
+            name: product.name,
+            price: product.price,
+            quantity: product.quantity,
+            cartQuantity: 1,
+        }
+        let cart = window.localStorage.getItem('cart');
+        cart = JSON.parse(cart)
+        cart.push(cartItem);
+        cart = JSON.stringify(cart)
+        window.localStorage.setItem("cart", cart)
+        navigate('/cart')
     }
 
     useEffect(() => {
@@ -59,8 +66,7 @@ const SingleProduct = () => {
             <h2>Name: {product.name}</h2>
             <h2>Price: {product.price}</h2>
             <h2>Description: {product.description}</h2>
-            <button onClick={(e) => handleAddToCart(userId, product.id)}>Add to Cart</button>
-            <button onClick={(e) => handleAddtoCartGuest()}>Add to Cart Guest</button>
+            {isLoggedIn ? <button id='add-to-user-cart' onClick={(e) => handleAddToCart(userId, product.id)}>Add to Cart</button> : <button id='add-to-guest-cart' onClick={(e) => handleAddtoCartGuest()}>Add to Cart</button>}
         </div>
     )
 }
