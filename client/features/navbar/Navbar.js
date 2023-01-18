dac746f50952c55d65966e672c6c4cbffcfa3ae8
Patrick Kenny <patrickmkenny91@gmail.com>
2023-01-18 14:48:10 -0600

    fixed name updated in top right corner when editing

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -2,17 +2,30 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../app/store';
+import { fetchUserAsync } from '../user/userSlice';
 
 const Navbar = () => {
   const { first_Name, last_Name } = useSelector((state) => state.auth.me);
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const isAdmin = useSelector((state) => state.auth.me.isAdmin);
+  const userId = useSelector((state) => state.auth.me.id);
+  const user = useSelector((state) => state.user)
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+
   const logoutAndRedirectHome = () => {
     dispatch(logout());
     navigate('/login');
   };
+
+
+ 
+  useEffect(() => {
+
+    
+
+  },[dispatch] )
 
   return (
     <div className="top">
@@ -34,7 +47,9 @@ const Navbar = () => {
         {isLoggedIn ? (
           <div>
             {/* The navbar will show these links after you log in */}
-            <span>Welcome {first_Name} {last_Name} </span>
+            {user.first_Name && user.last_Name ? <span>Welcome {user.first_Name} {user.last_Name}</span> : <span>Welcome {first_Name} {last_Name}</span> }
+
+          
             <button className='logOutButton' type="button" onClick={logoutAndRedirectHome}>
               Logout
             </button>
