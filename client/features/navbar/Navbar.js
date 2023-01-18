b3d8d564a4593a0a5307d5cfc85545cbde100d2d
Kevin Taylor <kevintaylor&84@gmail.com>
2023-01-17 20:41:32 -0500

    working on CSS

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = () => {
           <div className='userInfo'>
             {/* The navbar will show these links before you log in */}
             <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+            <Link to="/signup" className='signup'>Sign Up</Link>
           </div>
         )}
       </nav>
