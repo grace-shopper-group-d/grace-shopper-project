12205ed9da4be1c929704fa1ea96a14a9fab3cbe
Kevin Taylor <kevintaylor&84@gmail.com>
2023-01-18 14:56:28 -0500

    css homepage

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -22,7 +22,6 @@ const Navbar = () => {
           <Link className='top-item' to="/home">Home</Link>
           <Link className='top-item' to="/products">Products</Link>
           <Link className='top-item' to="/cart">Cart</Link>
-          <Link className='top-item' to="/orders">Orders</Link>
           <Link className='top-item' to="/useredit">Edit</Link>
           {isLoggedIn && isAdmin ? (
             <div>
